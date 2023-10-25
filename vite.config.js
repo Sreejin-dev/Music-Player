a6db9dcf19149ba5f43ead4c513d5485561a7cac
Sreejin-dev <sreejincp84@gmail.com>
2023-10-25 21:18:03 +0530

    test vitee

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/music-player/',
-
+  build: {
+    outDir: 'dist', // Output directory for the production build (default: 'dist')
+  },
 });
