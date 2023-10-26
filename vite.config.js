3c77485620ef1fca6334b72794c2ccd3e12aef3c
Sreejin-dev <sreejincp84@gmail.com>
2023-10-26 15:52:09 +0530

    Fix React and Vite versions, update base configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Music-Player/', // Replace with your repository name
+  base: "/Music-Player/",
+  // Replace with your repository name
   build: {
-    outDir: 'dist', // Output directory for the production build
+    outDir: "dist", // Output directory for the production build
   },
 });
