8df35fdb55b8fb6df881fdc303d19bad6e772c50
Sreejin-dev <sreejincp84@gmail.com>
2023-11-13 15:58:44 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import About from "./components/About/About";
 import Award from "./components/Awardwinpage/Award";
 import Header from "./components/Header/Header";
 import Product from "./components/Products/Product";
-import Cjn from "./components/tests/Cjn";
 
 
 function App() {
